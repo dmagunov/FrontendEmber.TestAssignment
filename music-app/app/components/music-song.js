18343cdb8d66cc8c56e1be246f9609b10fb4552f
Dmitry Magunov <dmitry.magunov@metricly.com>
2020-12-02 18:40:57 +0200

    Add new song

--- music-app/app/components/music-song.js
+++ music-app/app/components/music-song.js
@@ -6,6 +6,12 @@ import { action } from '@ember-decorators/object';
 export default class MusicSong extends Component {
   isEdit = false;
 
+  didInsertElement() {
+    if (this.song.isNew) {
+      this.set('isEdit', true);
+    }
+  }
+
   @action
   toggleEdit() {
     this.toggleProperty('isEdit');
--- music-app/app/controllers/album.js
+++ music-app/app/controllers/album.js
@@ -1,9 +1,21 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend ({
+  store: service(),
+
   actions: {
     saveSong(song){
       return song.save();
+    },
+    addSong() {
+      this.store.createRecord('song', {
+        name: '',
+        duration: 0,
+        createdAt: Date.now(),
+        album: this.model
+      });
+      return false;
     }
   }
 });
