e7a8d19818e606048f09a55ba258c9a573e4a76a
Dmitry Magunov <dmitry.magunov@metricly.com>
2020-12-02 15:46:20 +0200

    Add routes

---
+++ music-app/app/routes/index.js
@@ -0,0 +1,10 @@
+import Route from '@ember/routing/route';
+
+export default class IndexRoute extends Route {
+
+  model() {
+    return this.store.findAll('album', {
+      include: 'songs'
+    });
+  }
+}
