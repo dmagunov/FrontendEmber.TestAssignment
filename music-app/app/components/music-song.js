dbd794daee493956fe63576392a408d074c5f423
Dmitry Magunov <dmitry.magunov@metricly.com>
2020-12-02 18:19:16 +0200

    Edit song

--- music-app/app/components/music-song.js
+++ music-app/app/components/music-song.js
@@ -1,6 +1,13 @@
 import Component from '@ember/component';
 import { classNames } from '@ember-decorators/component';
+import { action } from '@ember-decorators/object';
 
 @classNames('music-song')
 export default class MusicSong extends Component {
+  isEdit = false;
+
+  @action
+  toggleEdit() {
+    this.toggleProperty('isEdit');
+  }
 }
