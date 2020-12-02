95655928262abec214a41a52141c9ff9bc820478
Dmitry Magunov <dmitry.magunov@metricly.com>
2020-12-02 16:55:26 +0200

    Create music-album component

---
+++ music-app/app/components/music-album.js
@@ -0,0 +1,7 @@
+import Component from '@ember/component';
+import { classNames } from '@ember-decorators/component';
+
+@classNames('music-album')
+export default class MusicAlbum extends Component {
+
+}
